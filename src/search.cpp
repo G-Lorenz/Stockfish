954403b7d921f829b1c8ef5800106f0e7621902f
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2021-05-01 20:01:59 +0200

    Trying a time independent version
    coeff=109
    
    Bench: 4075325

--- src/search.cpp
+++ src/search.cpp
@@ -506,8 +506,8 @@ void Thread::search() {
           && !Threads.stop
           && !mainThread->stopOnPonderhit)
       {
-          double fallingEval = (3180 + 110 * (mainThread->bestPreviousScore - bestValue)
-                                    + 110 * (mainThread->iterValue[iterIdx] - bestValue)) / 8250.0;
+          double fallingEval = (3180 + 109 * (mainThread->bestPreviousScore - bestValue)
+                                    + 109 * (mainThread->iterValue[iterIdx] - bestValue)) / 8250.0;
           fallingEval = std::clamp(fallingEval, 0.5, 1.5);
 
           // If the bestMove is stable over several iterations, reduce time accordingly
