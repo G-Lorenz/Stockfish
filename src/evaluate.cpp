fcfe1c07a899fa8f01ab9d1b0ebf853955778806
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2021-05-28 23:06:08 +0200

    Tweak: 7
    Bench: 3841043

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -304,7 +304,7 @@ namespace {
   constexpr Score TrappedRook         = S( 55, 13);
   constexpr Score WeakQueenProtection = S( 14,  0);
   constexpr Score WeakQueen           = S( 56, 15);
-  constexpr Score WeakKingProtection  = S( 21,  0);
+  constexpr Score WeakKingProtection  = S( 7,  0);
 
 
 #undef S
