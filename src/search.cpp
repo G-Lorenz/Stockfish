b20423336d8a46a1455cda68434dcbdc66fc9c98
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2021-06-29 22:01:41 +0200

    Tweak
    depth<8
    
    see
    https://discord.com/channels/435943710472011776/743930025832742912/859521761028079676
    for further details.
    
    Bench: 4793174

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
 
     // Step 7. Futility pruning: child node (~50 Elo)
     if (   !PvNode
-        &&  depth < 9
+        &&  depth < 8
         &&  eval - futility_margin(depth, improving) >= beta
         &&  eval < VALUE_KNOWN_WIN) // Do not return unproven wins
         return eval;
