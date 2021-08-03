8fe15a938af7052dc43452acecebf6a74da31fc6
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2021-08-03 07:32:01 +0200

    Simplification
    Update all conthist also when inCheck.
    
    Bench: 5707892

--- src/search.cpp
+++ src/search.cpp
@@ -1680,9 +1680,6 @@ moves_loop: // When in check, search starts here
 
     for (int i : {1, 2, 4, 6})
     {
-        // Only update first 2 continuation histories if we are in check
-        if (ss->inCheck && i > 2)
-            break;
         if (is_ok((ss-i)->currentMove))
             (*(ss-i)->continuationHistory)[pc][to] << bonus;
     }
