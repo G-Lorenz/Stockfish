68e9591adf99e2e2fa596a0cf347dffdf4f1e017
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-01-04 12:45:34 +0100

    Simplify LMR Pv
    since ss->ttPv and PvNode overlaps
    
    Bench: 3456936

--- src/search.cpp
+++ src/search.cpp
@@ -1159,14 +1159,10 @@ moves_loop: // When in check, search starts here
       {
           Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
 
-          // Decrease reduction at some PvNodes (~2 Elo)
-          if (   PvNode
-              && bestMoveCount <= 3)
-              r--;
-
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
           if (   ss->ttPv
+              && bestMoveCount <= 3
               && !likelyFailLow)
               r -= 2;
 
