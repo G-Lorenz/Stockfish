010369e621decc38dc85615e157382b91e5dadb8
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-01-04 12:33:07 +0100

    Lon ttCheck idea, but decreasing reductions
    (my old "restoration tune" shows that captures and checks act in opposition)
    
    Bench: 4766773

--- src/search.cpp
+++ src/search.cpp
@@ -1180,8 +1180,11 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture || ttCheck)
+          if (ttCapture)
               r++;
+
+          if (ttCheck)
+              r--;
 
           ss->statScore =  thisThread->mainHistory[us][from_to(move)]
                          + (*contHist[0])[movedPiece][to_sq(move)]
