7990cfa406ff65c707b8f0ccc6084773111cbe1e
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-02-01 22:17:40 +0100

    Try opposite
    Bench: 4551242

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -107,7 +107,7 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  5 * PieceValue[MG][pos.piece_on(to_sq(m))]
+          m.value =  7 * PieceValue[MG][pos.piece_on(to_sq(m))]
                    +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
