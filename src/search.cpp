966d11e77bea010393db1511618ac27ffedcb0e2
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-02-22 19:57:06 +0100

    Tweak
    Bench: 5363700

--- src/search.cpp
+++ src/search.cpp
@@ -1555,7 +1555,7 @@ moves_loop: // When in check, search starts here
 
       if (   captureOrPromotion
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY
-          && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 6 * alpha )
+          && thisThread->captureHistory[pos.moved_piece(move)][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] < 4 * alpha )
           continue;
 
       // movecount pruning for quiet check evasions
