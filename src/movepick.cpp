c7de0a16aa67b6eaa64a2e4c27550a69e3b7d1a3
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-06 23:01:16 +0100

    Add variable bonus
    Bench: 6268459

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -108,10 +108,10 @@ void MovePicker::score() {
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
       {
-          m.value =  int(PieceValue[MG][pos.piece_on(to_sq(m))]) * 6
-                   + (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          m.value =  6 * PieceValue[MG][pos.piece_on(to_sq(m))]
+                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
           if (pos.gives_check(m))
-              m.value *= 2;
+              m.value += PieceValue[MG][pos.piece_on(to_sq(m))];
       }
 
       else if constexpr (Type == QUIETS)
