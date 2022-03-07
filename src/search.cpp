32c91acc22828bdf84e9f6084be05b2435cea304
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-07 18:37:31 +0100

    Use reduced depth search in razoring
    Bench: 6015498

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
         && depth <= 7
         && eval < alpha - 348 - 258 * depth * depth)
     {
-        value = qsearch<NonPV>(pos, ss, alpha - 1, alpha);
+        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 4, false);
         if (value < alpha)
             return value;
     }
