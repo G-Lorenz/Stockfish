7aea061e6905b7c92aad06aeeca3ace3129260e8
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-08 21:36:29 +0100

    depth - 5
    Bench: 7394328

--- src/search.cpp
+++ src/search.cpp
@@ -780,7 +780,7 @@ namespace {
         && depth <= 7
         && eval < alpha - 348 - 258 * depth * depth)
     {
-        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 3, false);
+        value = search<NonPV>(pos, ss, alpha - 1, alpha, depth - 5, false);
         if (value < alpha)
             return value;
     }
