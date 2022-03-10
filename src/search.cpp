0a72b7f4a65a54348a969d4746ad0ecb1f30b6ff
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-10 15:24:24 +0100

    Take 2
    Bench: 6448336

--- src/search.cpp
+++ src/search.cpp
@@ -1371,9 +1371,9 @@ moves_loop: // When in check, search starts here
         // Use value difference to improve quiet move ordering
         if (ttValue && is_ok(ss->currentMove) && !ss->inCheck && !priorCapture)
         {
-            int bonus = std::clamp(16 * int(-ttValue + bestValue), -2000, 2000);
-            thisThread->mainHistory[us][from_to((ss)->currentMove)] << bonus;
-	}
+            int bonus = std::clamp(8 * int(-ttValue + bestValue), -1000, 1000);
+            thisThread->mainHistory[us][from_to((ss)->currentMove)] << bonus;	
+        }
 
         tte->save(posKey, value_to_tt(bestValue, ss->ply), ss->ttPv,
                   bestValue >= beta ? BOUND_LOWER :
