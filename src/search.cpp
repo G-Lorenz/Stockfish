d312b1d5e3399fa608c9893eef824d3fc6d91514
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-11 18:48:01 +0100

    Be sure ttCapture in LMR is a capture
    Bench: 7185385

--- src/search.cpp
+++ src/search.cpp
@@ -1163,7 +1163,7 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture)
+          if (ttCapture && !pos.empty(to_sq(ttMove)))
               r++;
 
           // Decrease reduction at PvNodes if bestvalue
