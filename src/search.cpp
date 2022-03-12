9866109dd5f2f4e12129afd1af6100eaef6cc01d
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-12 13:06:16 +0100

    Pure capture in step14
    Bench: 6673268

--- src/search.cpp
+++ src/search.cpp
@@ -1007,7 +1007,7 @@ moves_loop: // When in check, search starts here
           // Reduced depth of the next LMR search
           int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
 
-          if (   captureOrPromotion
+          if (   (captureOrPromotion && !pos.empty(to_sq(move)))
               || givesCheck)
           {
               // Futility pruning for captures (~0 Elo)
