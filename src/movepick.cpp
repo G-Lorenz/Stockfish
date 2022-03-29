4c7614c7e5bf38ece929f236b2ee9653eead176e
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-03-29 10:02:00 +0200

    Score promotion first
    Bench: 6677333

--- src/movepick.cpp
+++ src/movepick.cpp
@@ -174,8 +174,11 @@ void MovePicker::score() {
 
   for (auto& m : *this)
       if constexpr (Type == CAPTURES)
-          m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
-                   +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
+          if (pos.empty(to_sq(m)))
+              m.value =  1 << 28;
+          else
+              m.value =  6 * int(PieceValue[MG][pos.piece_on(to_sq(m))])
+                       +     (*captureHistory)[pos.moved_piece(m)][to_sq(m)][type_of(pos.piece_on(to_sq(m)))];
 
       else if constexpr (Type == QUIETS)
           m.value =      (*mainHistory)[pos.side_to_move()][from_to(m)]
