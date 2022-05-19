153398cdfeff52cf9eabdc7dc9fa5a596ceb10f9
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-05-19 17:15:01 +0200

    Store directly `1.0 / (beta - alpha)`
    0.6% speedup (tested at depth 16 and depth 23 with gcc and clang)
    
    Bench: 6443331

--- src/search.cpp
+++ src/search.cpp
@@ -69,9 +69,9 @@ namespace {
   // Reductions lookup table, initialized at startup
   int Reductions[MAX_MOVES]; // [depth or moveNumber]
 
-  Depth reduction(bool i, Depth d, int mn, Value delta, Value rootDelta) {
+  Depth reduction(bool i, Depth d, int mn, Value delta, double inverseRootDelta) {
     int r = Reductions[d] * Reductions[mn];
-    return (r + 1463 - int(delta) * 1024 / int(rootDelta)) / 1024 + (!i && r > 1010);
+    return int(r + 1463 - 1024 * int(delta) * inverseRootDelta) / 1024 + (!i && r > 1010);
   }
 
   constexpr int futility_move_count(bool improving, Depth depth) {
@@ -599,7 +599,7 @@ namespace {
             return alpha;
     }
     else
-        thisThread->rootDelta = beta - alpha;
+        thisThread->inverseRootDelta = 1.0 / (beta - alpha);
 
     assert(0 <= ss->ply && ss->ply < MAX_PLY);
 
@@ -1010,7 +1010,7 @@ moves_loop: // When in check, search starts here
           moveCountPruning = moveCount >= futility_move_count(improving, depth);
 
           // Reduced depth of the next LMR search
-          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->rootDelta), 0);
+          int lmrDepth = std::max(newDepth - reduction(improving, depth, moveCount, delta, thisThread->inverseRootDelta), 0);
 
           if (   capture
               || givesCheck)
@@ -1150,7 +1150,7 @@ moves_loop: // When in check, search starts here
               || !capture
               || (cutNode && (ss-1)->moveCount > 1)))
       {
-          Depth r = reduction(improving, depth, moveCount, delta, thisThread->rootDelta);
+          Depth r = reduction(improving, depth, moveCount, delta, thisThread->inverseRootDelta);
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~3 Elo)
