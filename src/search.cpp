77fca98067da7d12745fc34777000e1ffa5341cc
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-07-09 18:42:25 +0200

    Increase reduction if capture
    Bench: 5392478

--- src/search.cpp
+++ src/search.cpp
@@ -1160,7 +1160,7 @@ moves_loop: // When in check, search starts here
               r += 2;
 
           // Increase reduction if ttMove is a capture (~3 Elo)
-          if (ttCapture)
+          if (ttCapture || capture)
               r++;
 
           // Decrease reduction for PvNodes based on depth
