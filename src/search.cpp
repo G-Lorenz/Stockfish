5e0ee6529d139ebc3ecd6d80f64bbdb49f4e08c6
Giacomo Lorenzetti <g.lorenz9@protonmail.com>
2022-10-10 10:06:21 +0200

    Remove PieceValue in futCapt
    Tweaking it seems neutral (at least at STC...)
    
    Bench: 4257150

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts here
                   && !PvNode
                   && lmrDepth < 7
                   && !ss->inCheck
-                  && ss->staticEval + 180 + 201 * lmrDepth + PieceValue[EG][pos.piece_on(to_sq(move))]
+                  && ss->staticEval + 180 + 201 * lmrDepth
                    + captureHistory[movedPiece][to_sq(move)][type_of(pos.piece_on(to_sq(move)))] / 6 < alpha)
                   continue;
 
